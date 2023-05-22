03e9fb3e67e057eec23ac798492ef4c8adc62e10
Daniel Fletcher <101961238+danielfletcher0@users.noreply.github.com>
2023-05-22 15:49:52 -0500

    Time: 118 ms (38.62%) | Memory: 21.9 MB (8.11%) - LeetSync

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,22 +1,21 @@
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {} # for mapping the occurences of the values
-        freq = [[] for i in range(len(nums) + 1)] # this is where we are going to map the number of occurrences (by index) -> values
-        tot = 0 # keep up with our k limit of values
-
-        for num in nums: # for loop to initialize our dictionary
-            count[num] = 1 + count.get(num, 0) 
         
-        for num, key in count.items(): # for loop for mapping the occurences to values in our frequence multi-array
-            freq[key].append(num) 
+        grouping = [[] for i in range(len(nums) + 1)]
+        counter = {}
+        mapp = 0
 
+        for num in nums:
+            counter[num] = 1 + counter.get(num, 0)
+        
 
-        ans = [] # our answer array
-        for vals in range(len(freq) - 1, 0, -1): # we need to loop from the back to start from the max occurences
-            for i in freq[vals]: # looping through the values in the array
-                if tot < k: # checks our k limit
-                    if len(freq[vals]) != 0: # if occurance list isn't empty
-                        ans.append(i) # add a value to answer array
-                        tot += 1 # increase our k amount
+        for key, val in counter.items():
+            grouping[val].append(key)
+        ans = []
+        print(len(grouping))
+        for i in range(len(grouping) - 1, -1 , -1):
+            for j in grouping[i]:
+                if mapp < k:
+                    ans.append(j)
+                    mapp += 1
         return ans
