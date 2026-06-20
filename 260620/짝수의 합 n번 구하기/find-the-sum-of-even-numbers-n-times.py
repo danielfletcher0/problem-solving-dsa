96f46b506c8fc5a119a0591c3470ef140e52a083
Daniel Fletcher <101961238+danielfletcher0@users.noreply.github.com>
2026-06-19 23:22:12 -0700

    [Passed] 짝수의 합 n번 구하기 (54ms, 16MB)

---
+++ 260620/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.py
@@ -0,0 +1,8 @@
+N = int(input())
+for _ in range(N):
+    a, b = map(int, input().split())
+    total = 0
+    for i in range(a, b + 1):
+        if i % 2 == 0:
+            total += i
+    print(total)
